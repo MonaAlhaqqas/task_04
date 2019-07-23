47e1df63c7c28e74217c04e2ca032df75e12cd3d
MonaAlhaqqas <mh9433@gmail.com>
2019-07-23 16:39:02 +0300

    change dateTime field

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-class Restaurant (models.Model):
+class Restaurant (models.Model): #calss name should always be singuler and start with Capital letter
     name = models.CharField(max_length=10)
     description = models.TextField()
-    opening_time = models.DateTimeField()
-    closing_time = models.DateTimeField()
+    opening_time = models.TimeField()
+    closing_time = models.TimeField()
 
     def __str__(self):
-     return self.name
+     return self.name , self.opening_time
